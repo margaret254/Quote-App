1c6634c2d52aabc383f228a866911606ffec4d54
margaret254 <maggiemwas91@gmail.com>
2019-10-27 23:35:14 +0300

    ng generate a class Quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quotes } from './quotes';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'My Quotes App';
+  quote:Quotes[] = [
+    {name:'Margaret', quote:'Hide Yourself in God,so when a man wants to find you he will have to go there first.'},
+    {name:'Serah', quote:'May we all become teachers of peace,and teach in the only way possible: by example.'}
+  ];
 }
---
+++ src/app/quotes.ts
@@ -0,0 +1,5 @@
+export class Quotes {
+  name:string;
+  quote:string;
+  
+}
