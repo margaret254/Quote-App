f83e59a233905c69788b4075e85a76a9136016be
margaret254 <maggiemwas91@gmail.com>
2019-10-28 22:30:34 +0300

    Add a constructor function

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,6 @@
 export class Quotes {
   showDescription: boolean;
-  constructor(public name: string,public quoteName:string,public author:string,public completeDate: Date){
+  constructor(public name: string,public quote:string,public author:string, public upvote: number, public downvote: number,public completeDate: Date){
     this.showDescription=false;
   }
 }
