98a78d661666366aebdb6a80c3c725e7b1b1ba94
margaret254 <maggiemwas91@gmail.com>
2019-10-28 22:29:20 +0300

    Add addTotal function for calculating the upvotes

--- src/app/quote-comp/quote-comp.component.ts
+++ src/app/quote-comp/quote-comp.component.ts
@@ -9,11 +9,24 @@ import { Quotes } from '../quotes';
 export class QuoteCompComponent implements OnInit {
 
   quotes:Quotes[] = [
-    new Quotes('Margaret','Hide Yourself in God,so when a man wants to find you he will have to go there first.','Rose.W.',new Date(2019,10,24)),
-    new Quotes('Serah', 'May we all become teachers of peace,and teach in the only way possible: by example.','Charles.G',new Date(2019,10,24))
+    new Quotes('Margaret: ','  Hide Yourself in God,so when a man wants to find you he will have to go there first.','Rose.W.',0,0,new Date(2019,10,24)),
+    new Quotes('Serah: ', '  May we all become teachers of peace,and teach in the only way possible: by example.','Charles.G',0,0,new Date(2019,10,24)),
+    new Quotes('Kush: ', '  Manners are there to be learnt by example not by rules.','Ann Wanjiku',0,0,new Date(2019,10.3))
   ];
 
-  deleteGoal(isComplete,index){
+  firstNum :number
+  lastNum :number
+  count: number
+
+  addNewquote(quote){
+    let quoteLength = this.quotes.length;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+    // console.log(quote);2019
+
+  }
+
+  deleteQuote(isComplete,index){
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete the Quote?`)
 
@@ -21,6 +34,29 @@ export class QuoteCompComponent implements OnInit {
         this.quotes.splice(index,1)
       }
     }
+  }
+
+  addTotal(mine,index){
+    if(mine){
+      this.quotes[index].upvote +=1;
+    }
+  }
+
+  adTotal(isDown,index){
+    if(isDown){
+      this.quotes[index].upvote +=1;
+    }
+  }
+
+  loopVote(){
+    this.firstNum = 0
+    this.lastNum = 0
+
+    for(this.count=0 ; this.count < this.quotes.length; this.count++){
+      this.lastNum = this.quotes[this.count].upvote;
+      if(this.lastNum > this.firstNum){this.firstNum = this.lastNum}
+    }
+    return this.firstNum
   }
 
   toggleDetails(index){
