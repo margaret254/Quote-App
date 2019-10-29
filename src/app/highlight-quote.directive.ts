468bf77ca1b390875c7d791ed5c0d359fe08fec6
margaret254 <maggiemwas91@gmail.com>
2019-10-29 09:20:38 +0300

    Add a body tag in index.html file

--- src/app/highlight-quote.directive.ts
+++ src/app/highlight-quote.directive.ts
@@ -1,12 +1,21 @@
-import { Directive,ElementRef } from '@angular/core';
+import { Directive,ElementRef,HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightQuote]'
 })
 export class HighlightQuoteDirective {
 
-  constructor(private elem:ElementRef) {
-    this.elem.nativeElement.style.backgroundColor='aqua';
-   }
 
-}
+  constructor(private elem:ElementRef){}
+
+  @HostListener("click") addTotal(){
+    this.highlight("highlight");
+  }
+  private highlight(action:string){
+    this.elem.nativeElement.style.backgroundColor = 'blue';
+  }
+
+  }
+
+
+
